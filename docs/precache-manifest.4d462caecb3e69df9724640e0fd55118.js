071eb06c6239c221b822970d2811a3fe9d169409
Pablo Eugenio <juan.eugenio@smartkode.mx>
2021-07-30 14:11:08 -0500

    Añado del build (docs)

---
+++ docs/precache-manifest.4d462caecb3e69df9724640e0fd55118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0eb8de09fac795a492ce38107ebb3e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e3967d59723bb787d88",
+    "url": "/static/css/main.c6156140.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3967d59723bb787d88",
+    "url": "/static/js/main.e2d0b2e1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
